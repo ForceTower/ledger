d982f026c46136220f6a92cee11fefdfae9dc636
João Sena <joaopaulo761@gmail.com>
2026-06-29 20:10:43 -0300

    Add iOS app scaffold under apps/ios
    Xcode-generated SwiftUI project (Ledger module) placed in apps/ios.

---
+++ apps/ios/Ledger/LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerApp.swift
+//  Ledger
+//
+//  Created by João Paulo Santos Sena on 29/06/26.
+//
+
+import SwiftUI
+
+@main
+struct LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
